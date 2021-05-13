92f57726ac069cf056140f4edc581231b6841da4
nttung <nttung240197@gmail.com>
2021-05-13 21:42:52 +0700

    update

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ import mongoose from "mongoose";
 import logger from "morgan";
 import dotenv from "dotenv"
 import mainRoutes from "./server/routes/main.js";
+import jwt from "jsonwebtoken";
 
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 3000;
 const url = process.env.DB_URL;
+const secret = process.env.SECRET;
 
 app.set("view engine", "ejs");
 app.set("views","./views");
@@ -32,3 +34,58 @@ app.get("/", (req, res) => {
     res.send("Abc");
 });
 app.use("/api/", mainRoutes);
+
+app.post("/login", function(req, res){
+    if (req.body.Username=="admin"&&req.body.Password=="123456"){
+        var account = {
+            "Username": "admin",
+            "Email": "admin@gmail.com"
+        }
+        jwt.sign({
+            exp: Math.floor(Date.now()/1000) + 60*60*24*30,
+            data: account
+        }, secret, function(err, token){
+            if(err) {
+                res.json({
+                    success: false,
+                    message: "Sign token fail.",
+                    token: []
+                });
+            }
+            else{
+                res.json({
+                    success: true,
+                    message: "Sign token successfully.",
+                    token: token
+                });
+            }
+        });
+    }
+    else {
+        res.json({
+            success: false,
+            message: "Login fail, please try again.",
+            token: "fail"
+        })
+    }
+
+});
+
+app.post("/verifyToken", function(req, res){
+    jwt.verify(req.body.Token, secret, function(err, decoded){
+        if(err){
+            res.json({
+                success: false,
+                message: "Wrong token.",
+                data: []
+            });
+        }
+        else{
+            res.json({
+                success: false,
+                message: "Verify token successfully.",
+                data: decoded
+            });
+        }
+    });
+});
