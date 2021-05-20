e227cc6e69a24d5b8aa1a149d5f30352ba6c7923
nttung <nttung240197@gmail.com>
2021-05-20 20:17:33 +0700

    update

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import userRoutes from "./routes/user.js";
 import jwt from "jsonwebtoken";
 import {createServer} from "http";
 import {Server} from "socket.io";
+import multer from "multer";
 
 dotenv.config();
 
@@ -24,12 +25,12 @@ app.use(bodyParse.urlencoded({extended: false}));
 
 const server = createServer(app);
 const io = new Server(server); 
-
 app.io = io;
 
 //app.listen được thay bằng server.listen(3000)
 server.listen(3000);
 
+//Connect to MonggoDB
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     .then(() => {
         console.log("Database connected");
@@ -37,6 +38,27 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCrea
     .catch((error) => {
         console.log("Error connecting to database: " + error.message);
     });
+
+//Setup Multer for upload file
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/upload')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now()  + "-" + file.originalname)
+    }
+});  
+const upload = multer({ 
+    storage: storage,
+    fileFilter: function (req, file, cb) {
+        console.log(file);
+        if(file.mimetype=="image/bmp" || file.mimetype=="image/png" || file.mimetype=="image/jpg" || file.mimetype=="image/jpeg"){
+            cb(null, true)
+        }else{
+            return cb(new Error('Only image are allowed!'))
+        }
+    }
+}).single("avatar");
 
 app.use("/api/", [courseRoutes, userRoutes]);
 
@@ -95,9 +117,42 @@ app.post("/verifyToken", function(req, res){
     });
 });
 
+app.post("/uploadFile",  function(req, res){
+    upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            console.log("A Multer error occurred when uploading.");
+            res.json({
+                result: 0,
+                data: "A Multer error occurred when uploading."
+            });
+        } else if (err) {
+            console.log("An unknown error occurred when uploading." + err);
+            res.json({
+                result: 0,
+                data: "A Multer error occurred when uploading." + err
+            });
+        }else{
+            console.log("Upload is okay");
+            console.log(req.file); // Thông tin file đã upload
+            res.json({
+                result: 0,
+                data: req.file.filename
+            });
+        }
+
+    });
+});
+
+
+
 io.on("connection", function(socket){
     console.log("New Id" + socket.id);
     socket.on("disconnect", function(){
         console.log("User disconnected");
     });
+    socket.on("C_Sendto_S", function(data){
+        console.log(data);
+        socket.broadcast.emit("S_Sendto_C", data)
+    });
+    
 });
